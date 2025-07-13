31d400b4437a36347bad519a86e15ba3d94c7ff4
Digvijay-x1 <hackerearthx1@gmail.com>
2025-07-13 18:18:53 +0530

    Add necessary dependencies to package-lock.json for Clerk and Expo Secure Store integrations

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import {Redirect} from "expo-router";
 
 export default function Index  () {
     return (
-        <Redirect href="/(tabs)" />
+        <Redirect href="/(auth)/sign-up" />
     );
 
 };
