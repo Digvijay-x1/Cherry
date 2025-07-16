2486c086ecbd57938807eab7605ef532dec9549a
Digvijay-x1 <hackerearthx1@gmail.com>
2025-07-16 23:31:07 +0530

    feat: add Clerk and Convex integration with user management
    - Created auth.config.ts for Clerk provider configuration.
    - Implemented http.ts to handle Clerk webhooks for user creation.
    - Defined schema.ts for user, post, like, comment, follow, notification, and bookmark tables.
    - Added TypeScript configuration in tsconfig.json.
    - Developed users.ts for user creation mutation.
    - Updated package-lock.json and package.json to include new dependencies (convex and svix).
    - Created ClerkAndConvexProvider.tsx to wrap the application with Clerk and Convex providers.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,25 @@
-import { Text, View , StyleSheet , Image } from "react-native";
+import {Text, View, StyleSheet, Image, TouchableOpacity} from "react-native";
 import {Link} from "expo-router";
+import {useAuth} from "@clerk/clerk-expo";
+
+// we have to confirm that the request is coming from clerk
+// if true, we have to listen for that event (user.create event)
+// if so register the user in the database
 
 
 export default function Index()  {
-
+  const {signOut} = useAuth();
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Petrick Bateman</Text>
         <Image source={{uri:"https://upload.wikimedia.org/wikipedia/en/5/52/American-psycho-patrick-bateman.jpg"}}
                style={{width: 200, height: 200, resizeMode:"cover"} }/>
         <Link style={styles.title} href={"/notification"}>GO to notification</Link>
+        <TouchableOpacity onPress={()=>{signOut()}}>
+            <Text style={styles.title}>
+                signOUt
+            </Text>
+        </TouchableOpacity>
     </View>
   );
 }
